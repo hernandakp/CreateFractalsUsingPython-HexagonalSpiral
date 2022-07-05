7eda23d0a1f89bb7e4ec04305312dd94fd5a799e
Hernanda Khoiriyah Putri <104390322+hernandakp@users.noreply.github.com>
2022-07-06 00:47:52 +0700

    Add files via upload

---
+++ 1 Fractals-code.py
@@ -0,0 +1,9 @@
+'Author: hernanda472@gmail.com'
+
+import turtle
+colors = ['red', 'purple', 'blue', 'green', 'yellow']
+for x in range (180):
+    turtle.pencolor(colors[x%5])
+    turtle.width(x/50+5)
+    turtle.forward(x)
+    turtle.left(60)
